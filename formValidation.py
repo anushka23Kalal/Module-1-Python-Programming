a5bcd7f9d7886f8481d613a4be3e26ace166a847
anushka23Kalal <anushkasampathkalal@gmail.com>
2026-02-03 10:06:31 +0530

    Add files via upload

--- formValidation.py
+++ formValidation.py
@@ -1,8 +1,25 @@
 import re
-print("#Validate Email ,Phone and Age")
-email=input("Enter the mail id:")
-pattern=r"^[a-zA-Z._]+@[a-zA-Z]+\.[a-zA-Z]{2,}$"
-if re.match(email,pattern):
-    print("Email match")
-else:
+print("#Validate Email#")
+email = input("Enter the mail id:")
+pattern = r"^[a-zA-Z0-9._]+@[a-zA-Z]+\.[a-zA-Z]{2,}$"
 
+if re.match(pattern, email):
+    print("Email valid")
+else:
+    print("Email invalid")
+
+print("#Validate Phone#")
+phone = input("Enter the Phone Number:")
+pattern2 = r"^[6-9]\d{9}$"
+if re.match(pattern2, phone):
+    print("Phone number is valid")
+else:
+    print("Phone number is invalid")
+
+print("#Age Validation#")
+age = input("Enter the age:")
+pattern3 = r"^\d{1,3}$"
+if re.match(pattern3, age):
+    print("Age Valid")
+else:
+    print("Age Invalid")
